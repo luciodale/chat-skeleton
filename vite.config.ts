1703d1ec12acb68e7c44d1a623bed3721b1c4dc7
Lucio D'Alessandro <lda@juxt.pro>
2025-11-03 16:46:37 +0100

    Update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -22,15 +22,17 @@ export default defineConfig({
         background_color: "#000000",
         theme_color: "#ffffff",
         lang: "en",
+        start_url: "index.html",
+        categories: ["productivity", "ai", "messaging"],
         icons: [
           {
-            src: "/favicon/web-app-manifest-192x192.png",
+            src: "favicon/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/favicon/web-app-manifest-512x512.png",
+            src: "favicon/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
